3ed3ab82f25db7aa49b39a998a98f6bf8764296a
YakovBilbil <yakov.bilbil@gmail.com>
2022-06-02 14:22:17 +0300

    button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,20 @@ function Card() {
   return (
     <li className="card">
       <img className="card__picture" src={image} alt={`A Card name`} />
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
+      <div className="card__written-content-block">
+        <p className="card__date"> November 4, 2020 </p>
+        <h3 className="card__title">
+          Everyone Needs a Special 'Sit Spot' in Nature
+        </h3>
+        <p className="card__text">
+          Ever since I read Richard Louv's influential book, "Last Child in the
+          Woods," the idea of having a special "sit spot" has stuck with me.
+          This advice, which Louv attributes to nature educator Jon Young, is
+          for both adults and children to find...
+        </p>
+        <p className="card__origin">treehugger</p>
+        <div className="card__like-flag"></div>
+      </div>
     </li>
   );
 }
